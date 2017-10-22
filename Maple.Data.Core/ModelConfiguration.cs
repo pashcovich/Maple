95694cafe4b2052726a8ad4df078528388ebf6f7
Peter Vietense <peter.vietense@hotmail.de>
2017-10-22 16:15:02 +0200

    starting to figure out EF code

---
+++ Maple.Data.Core/ModelConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Maple.Data.Core
+{
+    public static class ModelConfiguration
+    {
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            ConfigureCoachEntity(modelBuilder);
+            ConfigureMediaItemEntity(modelBuilder);
+        }
+
+        private static void ConfigureCoachEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MediaPlayer>()
+                .OwnsOne(p => p.Playlist);
+        }
+
+        private static void ConfigureMediaItemEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MediaItem>()
+                .HasOne(p => p.Playlist)
+                .WithMany(mediaItem => mediaItem.MediaItems)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Maple.Data.Core/PlaylistContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Maple.Data.Core
+{
+    public class PlaylistContext : DbContext
+    {
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<MediaItem> MediaItems { get; set; }
+        public DbSet<MediaPlayer> Mediaplayers { get; set; }
+
+        public PlaylistContext()
+            : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Maple.sqlite; Version=3; Pooling=True; Max Pool Size=100;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ModelConfiguration.Configure(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
